1f4e5323c8739764c3b77b22cb53b70adde677e3
polinom <pryftina@gmail.com>
2022-10-12 19:57:03 +0300

    First hw Tomcat test

---
+++ TomcatTest/src/main/java/TestServlet.java
@@ -0,0 +1,15 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1> HELLO WORLD !!!</h1>");
+    }
+}
